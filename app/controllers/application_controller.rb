64aeb494256ff8f7f13186a73f44b6605eed8596
Jonathan Ghandforoush <jonnyghan@gmail.com>
2020-04-27 22:01:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
      user = User.new(:username => params[:username], :password => params[:password])
  
-  if user.save =true
+  if user.save
     redirect "/login"
   else
     redirect "/failure"
