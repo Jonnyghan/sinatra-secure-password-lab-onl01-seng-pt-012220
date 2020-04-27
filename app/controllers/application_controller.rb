7c25fe416004af7255d9b0e59a58726cb03a40ce
Jonathan Ghandforoush <jonnyghan@gmail.com>
2020-04-27 21:38:28 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,7 @@ class ApplicationController < Sinatra::Base
   post "/login" do
     user = User.find_by(:username => params[:username])
   if user
-    redirect "/success"
+    redirect "/account"
   else
     redirect "/failure"
   end
